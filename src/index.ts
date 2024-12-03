b06af5c1009b038eb4c296b3786bc3c4baf490d5
Murat Gözel <murat@gozel.com.tr>
2024-12-03 04:01:09 +0300

    fix: apisec export

--- src/index.ts
+++ src/index.ts
@@ -1,34 +1,10 @@
 export { getLogger, type LogLevel, type Logger } from "./logger";
 export { getMatomoClient, type MatomoClient } from "./matomo";
-export { ApiSecClient } from "./apisec";
+export { apisec } from "./apisec";
 export {
     readDevice,
     type DeviceMinimumRequirements,
     type DeviceObject,
     type DeviceDetails,
 } from "./device";
-export {
-    os,
-    type OSBusiness,
-    type OSLanguage,
-    type OSWebsiteSettings,
-    type OSMediaManifest,
-    type OSStaticMessages,
-    type OSPage,
-    type GeoRegion,
-    type OSPageComponent,
-    type OSPageComponentSingleText,
-    type OSPageComponentDualText,
-    type OSPageComponentImageText,
-    type OSPageComponentNewsArticle,
-    type OSPageComponentEvent,
-    type OSPageComponentDownloads,
-    type OSPageComponentPhotoGallery,
-    type OSPageComponentYoutubeVideo,
-    type OSPageComponentYoutubeVideoUrlResolved,
-    type OSPageComponentQa,
-    type OSPageComponentLink,
-    type OSPageComponentListing,
-    type OSPageComponentCta,
-} from "./os";
 export { getIdbObject, type Idb } from "./idb";
